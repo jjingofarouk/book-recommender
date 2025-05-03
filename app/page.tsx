5b43b7c28f72964e3db1d381e742fec87c13ce92
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-03 21:35:29 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         <div className="text-center">
           <Image
             className="dark:invert mx-auto"
-            src="/favicom.jpg"
+            src="/favicon.jpg"
             alt="LitShelf logo"
             width={180}
             height={38}
