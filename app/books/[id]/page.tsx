b1623eee4e9c3a8fd395851577444f3abead0442
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-03 20:15:17 +0300

    Update page.tsx

--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -1,6 +1,6 @@
 // app/books/[id]/page.tsx
-import BookDetailsClient from "@/components/BookDetailsClient";
-import { getBookById, getRelatedBooks } from "@/lib/books";
+import BookDetailsClient from "../../components/BookDetailsClient";
+import { getBookById, getRelatedBooks } from "../../lib/books";
 
 export default function Page({ params }: { params: { id: string } }) {
   const book = getBookById(parseInt(params.id));
