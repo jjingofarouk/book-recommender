d3a78bdc81ac6cc0646e82d1ee303d8a87f4a44e
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-03 12:03:52 +0300

    Update Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="p-4 bg-foreground text-background text-center">
+      <p>© 2025 Book Recommender. All rights reserved.</p>
+    </footer>
+  );
+}
