7d28088f25e37a6bfdc5acfc5e176679ad21e6c1
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-03 20:57:05 +0300

    Update RecommendationForm.tsx

--- app/components/RecommendationForm.tsx
+++ app/components/RecommendationForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { getPersonalizedRecommendations } from "@/lib/books";
+import { getPersonalizedRecommendations } from "../lib/books";
 
 export function RecommendationForm({
   onSubmit,
