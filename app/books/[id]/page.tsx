921d488d5fd741adbf8a659c4934f0e80012225d
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-03 13:21:09 +0300

    Update page.tsx

--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -1,16 +1,21 @@
 import BookDetailsClient from "../../components/BookDetailsClient";
 import { getBookById, getRelatedBooks } from "../../lib/books";
 
-type PageProps = {
+interface PageProps {
   params: {
     id: string;
   };
-};
+}
 
-export default async function Page({ params }: PageProps) {
-  const book = getBookById(parseInt(params.id));
+export default function Page({ params }: PageProps) {
+  const book = getBookById(Number(params.id));
+  
   if (!book) {
-    return <div className="p-8 text-center text-[var(--foreground)]">Book not found</div>;
+    return (
+      <div className="p-8 text-center text-[var(--foreground)]">
+        Book not found
+      </div>
+    );
   }
 
   const relatedBooks = getRelatedBooks(book.genre, book.id);
